ffb16e10c1c2f2b681a83043082d0c2f5fc7486b
Herve Omgba <herveomgba@macbook-pro-de-herve.home>
2017-06-24 13:50:47 +0200

    Committer: Herve Omgba <herveomgba@macbook-pro-de-herve.home> 	modified:   form.ts

--- src/pages/form/form.ts
+++ src/pages/form/form.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 import { HomePage } from '../home/home';
+import { AuthProvider } from '../../providers/auth';
+import { DataProvider } from '../../providers/data';
+import firebase from 'firebase';
+
+import {AngularFire, FirebaseListObservable} from 'angularFire2';
 
 @Component({
   selector: 'page-form',
@@ -9,8 +14,20 @@ import { HomePage } from '../home/home';
 })
 export class FormPage {
   public rootPage: any = HomePage;
-  constructor(public navCtrl: NavController) {
+  nameInput: 'lolo';
+  myUser: any;
 
+  constructor(public navCtrl: NavController, private auth: AuthProvider, public dp: DataProvider) {
+    this.myUser = this.auth.user;
+    console.log(this.myUser);
   }
 
+
+
+  updateData(): void{
+    var path: string = 'users/'+this.myUser.$key+'/name';
+    this.dp.update(path,'chichi');//pour tester avec une valeur en dur
+    console.log(path);
+
+  }
 }
